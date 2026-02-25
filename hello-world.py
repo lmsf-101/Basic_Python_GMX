9fcc956399b2d25db5b2940ca4fd941fb3166ad7
lmsf-101 <lmsf_academia_24@outlook.com>
2026-02-25 13:33:00 -0600

    Create hello-world.py
    Primer commit y primer programa

---
+++ hello-world.py
@@ -0,0 +1,6 @@
+# Primer programa para almacenar dato en una variable, donde se guarda un nombre
+nombre = "Luis"
+# La impresion de datos en la consola se hace mediante la función de "print()"
+print("Hola Mundo")
+# Puedo imprimir lo que hay dentro de la variable mediante "print()" y el uso de la variable
+print(f"Mi nombre es {nombre}")
